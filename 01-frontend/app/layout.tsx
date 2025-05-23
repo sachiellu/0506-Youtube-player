e78c671b9fcbfa68e9fe99cdc9c346e64782d954
呂紹瑜 <83488247+sachiellu@users.noreply.github.com>
2025-05-23 15:13:30 +0800

    Update layout.tsx

--- 01-frontend/app/layout.tsx
+++ 01-frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import Link from 'next/link';
+import Script from 'next/script';
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -12,9 +13,35 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const GA_TRACKING_ID = 'G-BVLZXL823S';  
+  
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+        
+        {GA_TRACKING_ID && (
+          <>
+            <Script
+              strategy="afterInteractive" // 建議的策略，在頁面可交互後加載
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <Script
+              id="gtag-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname, // 確保路由變化時能正確追蹤 page_view
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
+      </body>
     </html>
   )
 }
